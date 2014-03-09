fbb4ea9bd4203b4b5c683ac2366cb0f407a6638c
iSergium@gmail.com <iSergium@gmail.com@84bf2303-21a1-dadb-d2c6-36ef06536159>
2014-03-09 20:34:21 +0000

    1.0

--- defaults/preferences/inspector.js
+++ defaults/preferences/inspector.js
@@ -1,4 +1,4 @@
-pref("extensions.4pda-inspector.interval", 5);
+pref("extensions.4pda-inspector.interval", 30);
 pref("extensions.4pda-inspector.click_action", 1);
 pref("extensions.4pda-inspector.button_fontsize", 8);
 pref("extensions.4pda-inspector.button_big_fontsize", 12);
