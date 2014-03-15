88d54ef4c341cd24252f0699516217af073265e1
iSergium@gmail.com <iSergium@gmail.com@84bf2303-21a1-dadb-d2c6-36ef06536159>
2014-03-15 22:54:03 +0000

    themes list

--- branches/exp/content/js/toolbar.js
+++ branches/exp/content/js/toolbar.js
@@ -1,31 +1,60 @@
 var iToolbar = {
 
-    panel: null,
-    labels: {
-        username: null
-    },
+	panel: null,
+	elements: {
+		usernameLabel: null,
+		themesList: null
+	},
 
-    init: function()
-    {
-        this.panel = cScript.winobj.getElementById('inspectorPanel');
+	init: function()
+	{
+		this.panel = cScript.winobj.getElementById('inspectorPanel');
 
-        this.labels.username = cScript.winobj.getElementById('inspectorPanelUsername');
-        this.labels.username.onclick = function() {
-            alert(user.id);
-        };
-    },
-    
-    bClick: function(parent)
-    {
-        if (user.auth) {
-            // alert(user.name);
-            if (!this.panel) {
-                this.init();
-            };
-            this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
-            this.labels.username.value = user.name;
-        } else {
-            // открыть страницу авторизации
-        }
-    }
+		this.elements.usernameLabel = cScript.winobj.getElementById('inspectorPanelUsername');
+		this.elements.usernameLabel.onclick = function() {
+			alert(user.id);
+		};
+		this.elements.themesList = cScript.winobj.getElementById('inspectorThemesList');
+	},
+	
+	bClick: function(parent)
+	{
+		if (user.id) {
+			if (!this.panel) {
+				this.init();
+			};
+			this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
+			this.elements.usernameLabel.value = user.name;
+			this.clearThemesList();
+			this.printThemesList();
+		} else {
+			// открыть страницу авторизации
+		}
+	},
+
+	clearThemesList: function()
+	{
+		var labels = this.elements.themesList.getElementsByTagName('label');
+
+		for (var i = 0, length = labels.length; i < length; i++) {
+			labels[0].remove();
+		};
+	},
+
+	printThemesList: function()
+	{
+		var labels = iToolbar.elements.themesList.getElementsByTagName('label');
+		
+		for (var i = 0; i < themes.list.length; i++) {
+			iToolbar.elements.themesList.appendChild(iToolbar.createThemeRow(themes.list[i]));
+		};
+
+	},
+
+	createThemeRow: function(theme)
+	{
+		var newLabel = document.createElement('label');
+		newLabel.setAttribute('value', utils.htmlspecialcharsdecode(theme.title));
+		return newLabel;
+	}
 }
