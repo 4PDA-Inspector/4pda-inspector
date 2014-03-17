db7ae340cfcc579dc9129dffd886f2508f4b84f4
iSergium@gmail.com <iSergium@gmail.com@84bf2303-21a1-dadb-d2c6-36ef06536159>
2014-03-17 22:46:52 +0000

    panel

--- branches/exp/content/js/toolbar.js
+++ branches/exp/content/js/toolbar.js
@@ -23,38 +23,67 @@ var iToolbar = {
 			if (!this.panel) {
 				this.init();
 			};
-			this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
 			this.elements.usernameLabel.value = user.name;
-			this.clearThemesList();
 			this.printThemesList();
+			this.panel.openPopup(parent, 'after_start', 0, 0, false, true);
 		} else {
 			// открыть страницу авторизации
 		}
 	},
 
+	hidePanel: function()
+	{
+		this.clearThemesList();
+	},
+
 	clearThemesList: function()
 	{
-		var labels = this.elements.themesList.getElementsByTagName('label');
+		var labels = this.elements.themesList.getElementsByClassName('oneTheme');
 
-		for (var i = 0, length = labels.length; i < length; i++) {
-			labels[0].remove();
-		};
+		for (var i = labels.length - 1; i >= 0; i--) {
+			labels[i].remove();
+		}
 	},
 
 	printThemesList: function()
 	{
-		var labels = iToolbar.elements.themesList.getElementsByTagName('label');
-		
 		for (var i = 0; i < themes.list.length; i++) {
 			iToolbar.elements.themesList.appendChild(iToolbar.createThemeRow(themes.list[i]));
 		};
-
 	},
 
 	createThemeRow: function(theme)
 	{
-		var newLabel = document.createElement('label');
-		newLabel.setAttribute('value', utils.htmlspecialcharsdecode(theme.title));
-		return newLabel;
+		var themeCaptionLabel = document.createElement('label');
+		themeCaptionLabel.setAttribute('value', utils.htmlspecialcharsdecode(theme.title));
+		themeCaptionLabel.setAttribute('data-id', theme.id);
+		themeCaptionLabel.className = 'oneTheme_caption'
+
+		var userCaptionLabel = document.createElement('label');
+		userCaptionLabel.setAttribute('value', utils.htmlspecialcharsdecode(theme.last_user_name));
+		userCaptionLabel.setAttribute('data-id', theme.last_user_id);
+		userCaptionLabel.className = 'oneTheme_user'
+
+		var lastPostLabel = document.createElement('label');
+		lastPostLabel.setAttribute('value', new Date(theme.last_post_ts*1000).toLocaleString());
+		themeCaptionLabel.setAttribute('data-theme', theme.id);
+		lastPostLabel.className = 'oneTheme_lastPost'
+
+		// BOXES
+
+		var infoHBox = document.createElement('hbox');
+		infoHBox.className = 'oneThemeInfoHBox';
+		infoHBox.appendChild(userCaptionLabel);
+		infoHBox.appendChild(lastPostLabel);
+
+		var mainHBox = document.createElement('hbox');
+		mainHBox.appendChild(themeCaptionLabel);
+		
+		var themeVBox = document.createElement('vbox');
+		themeVBox.className = 'oneTheme';
+		themeVBox.appendChild(mainHBox);
+		themeVBox.appendChild(infoHBox);
+
+		return themeVBox;
 	}
 }
