04d282c55435986bd459244e7cf464b1cc14c31c
iSergium@gmail.com <iSergium@gmail.com@84bf2303-21a1-dadb-d2c6-36ef06536159>
2014-03-19 23:21:08 +0000

    icons

--- content/js/classes/themes.js
+++ content/js/classes/themes.js
@@ -19,13 +19,14 @@ var themes = {
 
 	parse: function(text) {
 		themes.list = [];
-		utils.log('themes');
+		// utils.log('themes');
 		var tText = text.replace('\r','').split('\n');
 		for (var i = 0; i < tText.length; i++) {
 			if (tText[i]) {
 				var theme = Object.create(themeObj);
-				theme.parse(tText[i]);
-				themes.list.push(theme);
+				if (theme.parse(tText[i])) {
+					themes.list.push(theme);
+				}
 			}
 		}
 	},
@@ -34,9 +35,29 @@ var themes = {
 		utils.openPage('http://4pda.ru/forum/index.php?showtopic='+id+'&view=getnewpost');
 	},
 
+	read: function(id) {
+		var xmr = Object.create(iXMR);
+		xmr.send('http://4pda.ru/forum/index.php?showtopic='+id+'&view=getnewpost');
+	},
+
 	openLast: function(id) {
 		utils.openPage('http://4pda.ru/forum/index.php?showtopic='+id+'&view=getlastpost');
-	}
+	},
+
+	openAll: function() {
+		while (themes.list.length) {
+			var theme = themes.list.splice(0,1)[0];
+			themes.open(theme.id);
+		}
+	},
+
+	readAll: function() {
+		while (themes.list.length) {
+			var theme = themes.list.splice(0,1)[0];
+			utils.log(theme, true);
+			themes.read(theme.id);
+		}
+	},
 }
 
 var themeObj = {
@@ -49,14 +70,19 @@ var themeObj = {
 	last_read_ts: '',
 
 	parse: function(text) {
-		var obj = utils.parse(text);
-		this.id = obj[0];
-		this.title = obj[1];
-		this.posts_num = obj[2];
-		this.last_user_id = obj[3];
-		this.last_user_name = obj[4];
-		this.last_post_ts = obj[5];
-		this.last_read_ts = obj[6];
+		try {
+			var obj = utils.parse(text);
+			this.id = obj[0];
+			this.title = obj[1];
+			this.posts_num = obj[2];
+			this.last_user_id = obj[3];
+			this.last_user_name = obj[4];
+			this.last_post_ts = obj[5];
+			this.last_read_ts = obj[6];
+		} catch(e) {
+			return false;
+		}
+		return this;
 		// tid title posts_num last_user_id last_user_name last_post_ts last_read_ts
 	}
 }
