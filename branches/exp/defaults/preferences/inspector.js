04819df60c93021a40855d8e12dc1fe525de2e87
iSergium@gmail.com <iSergium@gmail.com@84bf2303-21a1-dadb-d2c6-36ef06536159>
2014-03-21 23:07:51 +0000

    vars, autoupdate

---
+++ branches/exp/defaults/preferences/inspector.js
@@ -0,0 +1,5 @@
+pref("extensions.4pda-inspector.interval", 30);
+
+pref("extensions.4pda-inspector.notification_sound", true);
+pref("extensions.4pda-inspector.notification_popup", true);
+pref("extensions.4pda-inspector.notification_sound_volume", 100);
