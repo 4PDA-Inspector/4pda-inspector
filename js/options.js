7e8e59b0467c9765c8cab4bbfea9e31b1f21c599
iSergium <isergium@gmail.com>
2021-06-09 21:10:47 +0500

    manifest.json

--- js/options.js
+++ js/options.js
@@ -1,4 +1,4 @@
-const urlRegexp = /^(https?:\/\/)4pda\.ru([\/\w\.-\?\=\&\#]*)*\/?$/;
+const urlRegexp = /^(https?:\/\/)4pda\.to([\/\w\.-\?\=\&\#]*)*\/?$/;
 options = {
     bg: null,
     inputs: null,
@@ -114,7 +114,7 @@ options = {
     },
 
     printNotificationSoundVolume: function(value) {
-        document.getElementById('inspector4pda_notificationSoundVolumeLabel').textContent = (value * 100) + '%';
+        document.getElementById('inspector4pda_notificationSoundVolumeLabel').textContent = Math.round(value * 100) + '%';
     },
 
     setValue: function(name, value) {
