cced51f6d1125f40a61bffc12d67ed8b3818b151
iSergium <isergium@gmail.com>
2025-06-14 03:22:33 +0500

    ref

--- js/cs.js
+++ js/cs.js
@@ -59,21 +59,19 @@ class UnauthorizedError extends Error {};
 
 
 export class CS {
-    #initialized;
-    #cookie_authorized;
+    #initialized = false;
     #update_in_process = false;
+    #timeout_id = null;
+
+    #cookie_authorized = false;
+    #available = true;
+
+    #user_id = 0;
+    #user_name = '';
+    #last_event = 0;
 
     constructor() {
         console.log('Start CS', getLogDatetime());
-
-        this.#initialized = false;
-        this.#cookie_authorized = false;
-        this.timeout_id = null;
-
-        this.available = true;
-        this.user_id = 0;
-        this.user_name;
-        this.last_event = 0;
 
         this.favorites = new Favorites(this);
         this.qms = new QMS(this);
@@ -99,7 +97,6 @@ export class CS {
                         console.debug('Settings are saved');
                     });
                 }
-
             })
             .then(() => {
                 return this.#get_cookie_member_id()
@@ -118,12 +115,12 @@ export class CS {
                                         this.update();
                                     } else {
                                         console.debug('! Auth lost');
-                                        clearTimeout(this.timeout_id);
+                                        clearTimeout(this.#timeout_id);
                                         this.#do_logout();
                                         this.#cookie_authorized = false;
                                     }
                                 });
-                        }, 2000);
+                        }, 1000);
 
                         this.#cookie_authorized  = start_member_id != null;
                         return this.#cookie_authorized
@@ -153,8 +150,8 @@ export class CS {
     }
 
     #do_logout() {
-        this.user_id = 0;
-        this.user_name = '';
+        this.#user_id = 0;
+        this.#user_name = '';
         print_logout();
     }
 
@@ -163,18 +160,19 @@ export class CS {
             console.debug('SET INTERVAL: Update conflict. Skip.')
             return;
         }
-        clearTimeout(this.timeout_id);
+        clearTimeout(this.#timeout_id);
         this.update();
     }
 
-    get initialized() {
-        return this.#initialized;
-    }
+    get initialized() { return this.#initialized; }
+    get available() { return this.#available; }
+    get user_id() { return this.#user_id; }
+    
 
     get popup_data() {
         return {
-            user_id: this.user_id,
-            user_name: this.user_name,
+            user_id: this.#user_id,
+            user_name: this.#user_name,
             favorites: {
                 count: this.favorites.count,
                 list: this.favorites.list
@@ -196,7 +194,7 @@ export class CS {
     }
 
     async update() {
-        console.debug('* Start new update:', getLogDatetime(), this.timeout_id);
+        console.debug('* Start new update:', getLogDatetime(), this.#timeout_id);
         if (!this.#cookie_authorized) {
             console.debug('Hasn\'t cookie. Skip.')
             return;
@@ -207,21 +205,21 @@ export class CS {
         }
         let next_interval = SETTINGS.interval * 1000;
         this.#update_in_process = true;
-        this.available = true;
+        this.#available = true;
 
         return fetch4('https://4pda.to/forum/index.php?act=inspector&CODE=id')
             .then(data => {
                 let user_data = parse_response(data);
                 if (user_data && user_data.length == 2) {
-                    if (user_data[0] == this.user_id) {
-                        this.user_name = user_data[1];
-                        return this.#update();
+                    if (user_data[0] == this.#user_id) {
+                        this.#user_name = user_data[1];
+                        return this.#update_all_data();
                     } else {
-                        this.user_id = user_data[0];
-                        this.user_name = user_data[1];
-                        console.debug('New user:', this.user_id, this.user_name);
-                        this.last_event = 0;
-                        return this.#update(false);
+                        this.#user_id = user_data[0];
+                        this.#user_name = user_data[1];
+                        console.debug('New user:', this.#user_id, this.#user_name);
+                        this.#last_event = 0;
+                        return this.#update_all_data(false);
                     }                    
                 } else {
                     throw new UnauthorizedError('User ID not found');
@@ -232,14 +230,14 @@ export class CS {
                     console.debug('Unauthorized:', error.message);
                     this.#do_logout();
                 } else {
-                    this.available = false;
+                    this.#available = false;
                     print_unavailable();
                     console.error('API request failed:', error);
                     next_interval = 5000;
                 }
             })
             .finally(() => {
-                this.timeout_id = setTimeout(
+                this.#timeout_id = setTimeout(
                     () => this.update(),
                     next_interval
                 );
@@ -247,9 +245,9 @@ export class CS {
             });
     }
 
-    async #update(notify = true) {
+    async #update_all_data(notify = true) {
         return fetch(
-            `https://appbk.4pda.to/er/u${this.user_id}/s${this.last_event}`,
+            `https://appbk.4pda.to/er/u${this.#user_id}/s${this.#last_event}`,
             {
                 method: 'GET',
                 signal: AbortSignal.timeout(FETCH_TIMEOUT),
@@ -261,7 +259,7 @@ export class CS {
                     let parsed = data.match(PARSE_APPBK_REGEXP);
                     if (parsed) {
                         console.debug('! Has new events');
-                        this.last_event = parsed[1];
+                        this.#last_event = parsed[1];
                         return Promise.all([
                             this.favorites.update(notify),
                             this.qms.update(notify),
